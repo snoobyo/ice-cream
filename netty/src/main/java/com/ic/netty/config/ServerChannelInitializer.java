3101763cb6cb38c2764052609136c22f0acc15da
lxz <liuxianzhie@qq.com>
2021-06-01 12:25:26 +0800

    新增：netty基础应用

---
+++ netty/src/main/java/com/ic/netty/config/ServerChannelInitializer.java
@@ -0,0 +1,19 @@
+package com.ic.netty.config;
+
+import com.ic.netty.handler.NettyServerHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+public class ServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        //添加编解码
+        socketChannel.pipeline().addLast("decoder", new StringDecoder(CharsetUtil.UTF_8));
+        socketChannel.pipeline().addLast("encoder", new StringEncoder(CharsetUtil.UTF_8));
+        socketChannel.pipeline().addLast(new NettyServerHandler());
+    }
+}
---
+++ netty/src/main/java/com/ic/netty/handler/NettyServerHandler.java
@@ -0,0 +1,80 @@
+package com.ic.netty.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    // channelId <--> ctx
+    private static Map<String, ChannelHandlerContext> deviceOnlineMap = new ConcurrentHashMap<>();
+
+    // deviceId <--> channelId
+    private static Map<String, String> deviceControlMap = new ConcurrentHashMap<>();
+
+    /**
+     * 客户端连接会触发
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//        String channelId = ctx.channel().id().toString();
+//        map.put(channelId, ctx);
+//        System.out.println(map.size() + " | " + map);
+//        log.info(channelId+ "Channel active......");
+        int port = ((InetSocketAddress) ctx.channel().localAddress()).getPort();
+        System.out.println(port);
+        System.out.println("连接：" + Thread.currentThread().getName());
+
+    }
+
+    /**
+     * 客户端发消息会触发
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("接收消息：" + Thread.currentThread().getName());
+//        if (true) {
+//            throw new RuntimeException("");
+//        }
+//        String str = null;
+//        str.toString();
+        log.info("服务器收到消息: {}", msg.toString());
+//        ctx.write("你也好哦");
+//        ctx.flush();
+    }
+
+    /**
+     * 发生异常触发
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    /**
+     * 客户端断开会触发
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+//        String channelId = ctx.channel().id().toString();
+//        map.remove(channelId);
+//        System.out.println(map.size() + " | " + map);
+//        log.info(channelId + "Channel inactive......");
+        log.info("Channel inactive......");
+    }
+}
+
+//map<channelId, ctx>
+//
+//map<deviceId, channelId>
+
+
+
