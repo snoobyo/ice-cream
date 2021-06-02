2789b013f74fd6be34297dbf3f22bc4b8272796e
lxz <liuxianzhie@qq.com>
2021-06-02 17:48:19 +0800

    新增：xml解析工具类

--- common/src/main/java/com/ic/common/util/XmlUtil.java
+++ common/src/main/java/com/ic/common/util/XmlUtil.java
@@ -1,6 +1,5 @@
 package com.ic.common.util;
 
-import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
@@ -8,40 +7,36 @@ import org.dom4j.io.SAXReader;
 
 import java.io.File;
 import java.util.Iterator;
-import java.util.List;
 
 public class XmlUtil {
 
     public static void parse(String filePath) {
-        //1.创建Reader对象
+        // 1.创建Reader对象
         SAXReader reader = new SAXReader();
-        //2.加载xml
+        // 2.加载xml
         Document document = null;
         try {
-            document = reader.read(new File("src/main/resources/demo.xml"));
+            document = reader.read(new File(filePath));
         } catch (DocumentException e) {
             e.printStackTrace();
         }
-        //3.获取根节点
+        // 3.获取根节点
         Element rootElement = document.getRootElement();
-        Iterator iterator = rootElement.elementIterator();
+        System.out.println(rootElement.getName());
+        recursion(rootElement);
+    }
+
+    public static void recursion(Element element) {
+        Iterator iterator = element.elementIterator();
         while (iterator.hasNext()) {
-            Element stu = (Element) iterator.next();
-            List<Attribute> attributes = stu.attributes();
-            System.out.println("======获取属性值======");
-            for (Attribute attribute : attributes) {
-                System.out.println(attribute.getValue());
-            }
-            System.out.println("======遍历子节点======");
-            Iterator iterator1 = stu.elementIterator();
-            while (iterator1.hasNext()) {
-                Element stuChild = (Element) iterator1.next();
-                System.out.println("节点名："+stuChild.getName()+"---节点值："+stuChild.getStringValue());
-            }
+            Element nextElement = (Element) iterator.next();
+            System.out.println(nextElement.getName());
+            recursion(nextElement);
         }
     }
 
     public static void main(String[] args) {
-//        parse();
+        parse("/Users/wind/Desktop/test.xml");
+//        parse("/Users/wind/Desktop/演示一线#10.kml");
     }
 }
